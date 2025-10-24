0c28dd895143c7a4a0e082ab9136a49e7b329c34
Agarvv <casluagarv@gmail.com>
2025-10-24 20:19:07 +0200

    discord.log off

--- main.py
+++ main.py
@@ -2,23 +2,37 @@ import discord
 from discord.ext import commands
 import logging 
 import os 
-
-
 from dotenv import load_dotenv 
+from flask import Flask
+from threading import Thread
 
 load_dotenv() 
-
 token = os.getenv('DISCORD_TOKEN')
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf8', mode='w')
+
 intents = discord.Intents.default()
 intents.message_content = True 
-
 bot = commands.Bot(command_prefix='/', intents=intents) 
 
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "OK"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
 @bot.event
 async def on_ready():
-    print("Runs")
-    
+    print("Bot running!")
+
+
+keep_alive()
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)

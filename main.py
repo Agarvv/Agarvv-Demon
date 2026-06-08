63b399a140d0af72955e1afef145f147b0f5daf9
Agarv <162643502+Agarvv@users.noreply.github.com>
2026-06-08 23:52:24 +0000

    welcome

--- main.py
+++ main.py
@@ -44,80 +44,24 @@ async def abort(s):
     await channel.send(f"Aborte a mi pene por error en: {s}")
     while(1 > 0):
         await asyncio.sleep(3600)
+
+spam_channel = bot.get_channel(890963484181954610) 
+general_channel = bot.get_channel(997124912475021462)
+s = 0 
     
 @bot.event
 async def on_ready():
-    guild = bot.guilds[0]
-    start = datetime(2026, 5, 1)
-    end = datetime(2026, 5, 31, 23, 59, 59)
-    r = 0
-    saved = []
-    channel = bot.get_channel(890963484181954610);
-    await channel.send("<&1183086057344991263> no soy un monstruo gorda puta. yo no abortare a mis hijos como tu abortaste a tado, osea... panza, panza, panza, panza. mileurista, mileurista, mileurista. es como.. fuck. yo no puedo aguantar mucho mas aqui, ¿sabes?")
-    print("hola")
-        
-    
-    try:  
-        for channel in guild.text_channels:
-            try: 
-                async for message in channel.history(after=start, before=end, limit=None):
-                    print(message.created_at)
-                    if message.author.id not in saved:
-                        saved.append(message.author.id)
-
-                    if r >= 1000:
-                        r = 0
-                        await asyncio.sleep(1)
-                        
-                    else:
-                        r += 1
-
-            except Exception as e:
-                print(f"error3: {type(e).__name__} - {str(e)}")  
-                await channel.send("error de no seq nosecuanto0")
-                while(1 > 0): 
-                    await asyncio.sleep(3600)
-
-    except:
-        print(f"error3: {type(e).__name__} - {str(e)}")  
-        print("error3")
-        await channel.send("error de no seq nosecuanto")
-        while(1 > 0): 
-            await asyncio.sleep(3600)
-    
-    print("pollaaaaaa")
-    async for idd in saved:
-        user = await guild.get_member(idd) 
-        
-        printf(f"user: {user.username}")
-        
-    await channel.send(f"Se libraron {len(saved)} usuarios, sidiikslxcskl")
-    while(1 > 0): 
-        await asyncio.sleep(3600)
+    print("polla")
+    await general_channel.send("Hola me gusta el pito")
 
 
-    async for member in guild.fetch_members(limit=None):
-        if member.id not in saved and not member.bot:  
-            try:
-                await member.kick(reason="kick por inactivo.......... :v")
-                await asyncio.sleep(0.5)  
-            except:
-                abort("kick")
-            
-            
-            
-        
-        
 
 
-    try:
-        check_schedule.start()
-        print("✅ Task iniciada")
-    except Exception as e:
-        print(f"❌ Error iniciando task: {e}")
+import discord
 
-spam_channel = bot.get_channel(890963484181954610) 
-s = 0 
+@bot.event
+async def on_member_join(member):
+        await general_channel.send(f"¡Bienvenido {member.mention} al servidor, pasala de puta madre y no olvides beber agua y nunca tomar acido sulfurico bajo ningun concepto, y que karlita te acompanie en todas tus acciones.")
 
 @bot.event
 async def on_message(msg):
